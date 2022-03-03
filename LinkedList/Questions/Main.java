f83532f34df7e3bb0656a50d52d097a6229ee1f4
Tarso Jabbes <tarsojabbes@gmail.com>
2022-03-03 20:14:14 -0300

    Implents a linked list for questions resolution

---
+++ LinkedList/Questions/Main.java
@@ -0,0 +1,14 @@
+package LinkedList.Questions;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+
+        ll.createLinkedList(1);
+        ll.insertNode(2);
+        ll.insertNode(3);
+        ll.insertNode(4);
+        ll.insertNode(5);
+        ll.traverseLinkedList();
+    }
+}
---
+++ LinkedList/Questions/Node.java
@@ -0,0 +1,7 @@
+package LinkedList.Questions;
+
+public class Node {
+    public int value;
+    public Node next;
+    public Node previous;
+}
